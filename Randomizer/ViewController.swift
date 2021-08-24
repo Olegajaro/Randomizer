fba8dd26676356666dec91f39e7c742807ef5fdc
Olegajaro <88854903+Olegajaro@users.noreply.github.com>
2021-08-24 15:08:54 +0300

    Added logic for app

--- Randomizer/ViewController.swift
+++ Randomizer/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var minimumValueLabel: UILabel!
+    @IBOutlet var maximumLabelValue: UILabel!
+    @IBOutlet var randomValueLabel: UILabel!
+    
+    
+    @IBOutlet var getRandomNumberButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        getRandomNumberButton.layer.cornerRadius = 10
     }
 
-
+    
+    @IBAction func getRandomNumberButtonTapped() {
+        let minimumNumber = Int(minimumValueLabel.text ?? "") ?? 0
+        let maximumNumber = Int(maximumLabelValue.text ?? "") ?? 100
+        
+        randomValueLabel.text = String(Int.random(in: minimumNumber...maximumNumber))
+    }
+    
+    
 }
 

fbdb84f4aa4d7969a79a0fc1543cb4f65a88106c
Olegajaro <88854903+Olegajaro@users.noreply.github.com>
2021-08-24 15:48:56 +0300

    Added new class SettingsViewController.swift and linked it to the second view screen

---
+++ Randomizer/SettingsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsViewController.swift
+//  Randomizer
+//
+//  Created by Олег Федоров on 24.08.2021.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
